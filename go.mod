885c7594b8ac0cd63d6298faaf0c9ff741846665
Joao <joaodslourenco@gmail.com>
2024-03-23 14:05:23 -0300

    refactor: adjust to keep algos in separate folder

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module merge-sort
+module algos
 
 go 1.22.0
---
+++ merge_sort/merge_sort.go
@@ -0,0 +1,53 @@
+package sortAlgos
+
+import "fmt"
+
+func MergeSort(numbers []int) {
+	if len(numbers) <= 1 {
+		return
+	}
+
+	middleIndex := len(numbers) / 2
+
+	leftSlice := make([]int, middleIndex)
+	copy(leftSlice, numbers[:middleIndex])
+
+	rightSlice := make([]int, len(numbers)-middleIndex)
+	copy(rightSlice, numbers[middleIndex:])
+
+	fmt.Println("left", leftSlice, "right", rightSlice)
+
+	MergeSort(leftSlice)
+	MergeSort(rightSlice)
+
+	merge(leftSlice, rightSlice, numbers)
+}
+
+func merge(leftSlice, rightSlice, slice []int) {
+	i, l, r := 0, 0, 0
+
+	fmt.Println(slice, leftSlice, rightSlice)
+
+	for l < len(leftSlice) && r < len(rightSlice) {
+		if leftSlice[l] < rightSlice[r] {
+			slice[i] = leftSlice[l]
+			l++
+		} else {
+			slice[i] = rightSlice[r]
+			r++
+		}
+		i++
+	}
+
+	for l < len(leftSlice) {
+		slice[i] = leftSlice[l]
+		l++
+		i++
+	}
+	for r < len(rightSlice) {
+		slice[i] = rightSlice[r]
+		r++
+		i++
+	}
+	fmt.Println(slice)
+}
